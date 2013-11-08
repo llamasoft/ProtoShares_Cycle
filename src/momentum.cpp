4a6c81aed76f3dca2f0f0969139aa27005d1ba14
Llamasoft <gravisoft+git@gmail.com>
2013-11-08 12:00:39 -0600

    Initial Commit
    Using a turtle/hare cycle detection algorithm instead of memory to
    search for collision.
    Also, debug statements.

--- src/momentum.cpp
+++ src/momentum.cpp
@@ -1,90 +1,125 @@
 #include "momentum.h"
 #include <boost/unordered_map.hpp>
 #include <iostream>
-#include <semiOrderedMap.cpp>
 
-namespace bts 
+namespace bts
 {
-	#define MAX_MOMENTUM_NONCE  (1<<26)
-	#define SEARCH_SPACE_BITS 50
-	#define BIRTHDAYS_PER_HASH 8
-	
-	std::vector< std::pair<uint32_t,uint32_t> > momentum_search( uint256 midHash )
-  {
-			semiOrderedMap somap;
-			somap.allocate(4);
-      std::vector< std::pair<uint32_t,uint32_t> > results;
+#define MAX_MOMENTUM_NONCE  (1<<26)
+#define SEARCH_SPACE_BITS 50
+#define BIRTHDAYS_PER_HASH 8
 
-      char  hash_tmp[sizeof(midHash)+4];
-      memcpy((char*)&hash_tmp[4], (char*)&midHash, sizeof(midHash) );
-      uint32_t* index = (uint32_t*)hash_tmp;
+std::vector< std::pair<uint32_t, uint32_t> > momentum_search(uint256 midHash)
+{
+    std::vector< std::pair<uint32_t, uint32_t> > results;
 
-      for( uint32_t i = 0; i < MAX_MOMENTUM_NONCE;  )
-      {
-	      if(i%1048576==0){
-		boost::this_thread::interruption_point();
-	      }
-          *index = i;
-          uint64_t  result_hash[8];
-          SHA512((unsigned char*)hash_tmp, sizeof(hash_tmp), (unsigned char*)result_hash);
+    char  hash_tmp[sizeof(midHash) + 4];
+    memcpy((char *)&hash_tmp[4], (char *)&midHash, sizeof(midHash));
+    uint32_t *index = (uint32_t *)hash_tmp;
+    bool found_hit = false;
+    
+    // X
+    uint32_t turtle_nonce = 0;
+    uint32_t turtle_offset = 0;
+    uint64_t turtle[8]; 
+    
+    // X'
+    uint32_t hare_nonce = 0;
+    uint32_t hare_offset = 0;
+    uint64_t hare[8];
+    
+    // Defining X_0
+    *index = 0;
+    uint64_t result_hash[8];
+    SHA512((unsigned char *)hash_tmp, sizeof(hash_tmp), (unsigned char *)result_hash);
+    turtle = result_hash;
+    hare = result_hash;
+
+    
+    // Dig for a hit.
+    // We can optimize out the backtrack step
+    //    because we're keeping track of our nonces.
+    for(uint32_t i = 0; i < (1<<(SEARCH_SPACE_BITS/2) + 1); ++i) {
         
-	      
-          for( uint32_t x = 0; x < 8; ++x )
-          {
-              uint64_t birthday = result_hash[x] >> (64-SEARCH_SPACE_BITS);
-              uint32_t nonce = i+x;
-              //boost::unordered_map<uint64_t,uint32_t>::const_iterator itr = found.find( birthday );
+        // TURTLE
+        // X = H(X)
+        ++turtle_offset;
+        if (turtle_offset >= BIRTHDAYS_PER_HASH) {
+            turtle_offset = turtle_offset % BIRTHDAYS_PER_HASH;
+            turtle_nonce = (turtle_nonce + BIRTHDAYS_PER_HASH) % MAX_MOMENTUM_NONCE;
+            
+            *index = turtle_nonce;
+            SHA512((unsigned char *)hash_tmp, sizeof(hash_tmp), (unsigned char *)result_hash);
+            turtle = result_hash;
+        }
+        
+        
+        // HARE
+        // X' = H( H(X) )
+        hare_offset += 2;
+        if (hare_offset >= BIRTHDAYS_PER_HASH) {
+            hare_offset = hare_offset % BIRTHDAYS_PER_HASH;
+            hare_nonce = (hare_nonce + BIRTHDAYS_PER_HASH) % MAX_MOMENTUM_NONCE;
+            
+            *index = hare_nonce;
+            SHA512((unsigned char *)hash_tmp, sizeof(hash_tmp), (unsigned char *)result_hash);
+            hare = result_hash;
+        }
 
-              uint64_t foundMatch=somap.checkAdd( birthday, nonce );
-              if( foundMatch != 0 ){
-                  results.push_back( std::make_pair( foundMatch, nonce ) );
-              }
-          }
-          i += BIRTHDAYS_PER_HASH;
-      }
-      //for( auto itr = results.begin(); itr != results.end(); ++itr )
-      //{
-      //   assert( momentum_verify( midHash, itr->first, itr->second ) );
-     // }
-      //somap.destroy();
-      return results;
-   }
+        
+        // Found a collision!
+        if (turtle[turtle_offset] >> (64 - SEARCH_SPACE_BITS) == hare[hare_offset] >> (64 - SEARCH_SPACE_BITS) {
+            fount_hit = true;
+            break;
+        }
+    }
+    
+    // If we stopped due to running out of entries, return the empty list
+    if (!found_hit) {
+        std::cerr << "No collision found.\n";
+        return results;
+    }
+    
+    std::cerr << "Collision found!\n";
+    results.push_back( std::make_pair(turtle_nonce + turtle_offset, hare_nonce + hare_offset) );
+    return results;
+}
 
 
-	uint64_t getBirthdayHash(const uint256& midHash, uint32_t a)
-  {
-    uint32_t index = a - (a%8);
-    char  hash_tmp[sizeof(midHash)+4];
-  //  std::cerr<<"midHash size:" <<sizeof(midHash)<<"\n";
-    memcpy(&hash_tmp[4], (char*)&midHash, sizeof(midHash) );
-    memcpy(&hash_tmp[0], (char*)&index, sizeof(index) );
+uint64_t getBirthdayHash(const uint256 &midHash, uint32_t a)
+{
+    uint32_t index = a - (a % 8);
+    char  hash_tmp[sizeof(midHash) + 4];
+
+    memcpy(&hash_tmp[4], (char *)&midHash, sizeof(midHash));
+    memcpy(&hash_tmp[0], (char *)&index, sizeof(index));
 
     uint64_t  result_hash[8];
-//      for( uint32_t i = 0; i < sizeof(hash_tmp); ++i )
- //     {
-  //       std::cerr<<" "<<uint16_t((((unsigned char*)hash_tmp)[i]));
-   //   }
-//      std::cerr<<"\n";
-		SHA512((unsigned char*)hash_tmp, sizeof(hash_tmp), (unsigned char*)&result_hash);
-//    std::cerr<<"result_hash "<<a<<"  "<<a%8<<"  --- ";
- //   for( uint32_t i = 0; i < 8; ++i ) std::cerr<<result_hash[i]<<" ";
-  //  std::cerr<<"\n";
 
-    uint64_t r = result_hash[a%BIRTHDAYS_PER_HASH]>>(64-SEARCH_SPACE_BITS);
-  //  std::cerr<<"bdayresult: "<<r<<"\n";
+    SHA512((unsigned char *)hash_tmp, sizeof(hash_tmp), (unsigned char *)&result_hash);
+
+
+    uint64_t r = result_hash[a % BIRTHDAYS_PER_HASH] >> (64 - SEARCH_SPACE_BITS);
+
     return r;
-	}
+}
 
-	bool momentum_verify( uint256 head, uint32_t a, uint32_t b ){
- //   std::cerr<<"verify  "<<a<<"  and "<<b<<"  mid: "<<head.ToString()<<"\n";
- //   std::cerr<<"    "<<getBirthdayHash(head,a)<<"   "<<getBirthdayHash(head,b)<<"\n";
-		if( a == b ) return false;
-		if( a > MAX_MOMENTUM_NONCE ) return false;
-		if( b > MAX_MOMENTUM_NONCE ) return false;		
+bool momentum_verify(uint256 head, uint32_t a, uint32_t b)
+{
 
-     bool r = (getBirthdayHash(head,a) == getBirthdayHash(head,b));
-  //   std::cerr<< "####### Verified "<<int(r)<<"\n";
-     return r;
-	}
+    if(a == b) {
+        return false;
+    }
+
+    if(a > MAX_MOMENTUM_NONCE) {
+        return false;
+    }
+
+    if(b > MAX_MOMENTUM_NONCE) {
+        return false;
+    }
+
+    bool r = (getBirthdayHash(head, a) == getBirthdayHash(head, b));
+    return r;
+}
 
 }
